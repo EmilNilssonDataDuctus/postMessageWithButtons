7a3d187cfad83db47d686c9fa5b5f6146fb80623
Emil Nilsson <emil.nilsson@ductus.se>
2024-10-04 16:11:30 +0200

    Add wildcard link

--- windowPostMessageOrigin.js
+++ windowPostMessageOrigin.js
@@ -1,11 +1,15 @@
 const WINDOW_URL = "https://incandescent-frangollo-63d854.netlify.app/"
 const WINDOW_URL2 = "https://gilded-kulfi-696033.netlify.app/"
+const WINDOW_URL_WILDCARD = "https://astonishing-torrone-744d87.netlify.app/"
 
 function openSecondChannel() {
   window.open(WINDOW_URL);
 }
 function openSecondChannel2() {
   window.open(WINDOW_URL2);
+}
+function openSecondChannel3() {
+  window.open(WINDOW_URL_WILDCARD);
 }
 
 const channelRenderer = document.getElementById("channel-renderer");
